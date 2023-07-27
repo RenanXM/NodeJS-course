33cc90e01e2ec632094217d4403f6fe164c49178
RenanXM <renanxmarques303@gmail.com>
2023-07-27 16:28:28 -0300

    Finalizando PROJETO 1 - Perguntas e Respostas

--- 04guiaperguntas(project)/index.js
+++ 04guiaperguntas(project)/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 // Database
 connection
     .authenticate()
@@ -19,20 +20,76 @@ app.use(express.static('public'));
 // Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+
 // Rotas
 app.get("/",(req, res) => {
-    res.render("index");
-});
+    Pergunta.findAll({raw:true, order: [
+        ['id','DESC'] //ASC = crescente, DESC = decrescente
+    ]}).then(perguntas => {
+        res.render("index", { 
+            perguntas: perguntas
+        });
+    });
+}); 
+
 
 app.get("/perguntar",(req, res) => {
     res.render("perguntar");
 })
 
+// Criar pergunta
 app.post("/salvarpergunta",(req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+
+    // CREATE : insert INTO ...
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        // Caso sucesso: redireciona pra '/'
+        res.redirect("/")
+    });
 });
+
+// Página da Pergunta
+app.get("/pergunta/:id", (req,res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined) {
+
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order: [
+                    ['id', 'DESC']
+                ]
+
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+        } else {
+            res.redirect("/")
+        }
+    });
+});
+
+// Responder Pergunta
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId 
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId)
+    })
+})
 
 
 app.listen(8080,()=>{console.log("App rodando!");});
