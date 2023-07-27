e7c549ee7cd12f4ccd027897573b1701f014c83a
RenanXM <renanxmarques303@gmail.com>
2023-07-27 12:24:36 -0300

    corrigindo erros

--- 04guiaperguntas(project)/index.js
+++ 04guiaperguntas(project)/index.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-//Dizendo para o Express usar o EJS como View Engine
-app.set('view engine', 'ejs');
-
-
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-    //passando valores pro meu html(ejs)
-    //<%= %> //exibir valor da variável.
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa:"Renan's Tecnologia",
-        inscritos: 8040,
-        msg: exibirMsg
-    });
+// Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine','ejs');
+app.use(express.static('public'));
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Rotas
+app.get("/",(req, res) => {
+    res.render("index");
 });
 
-app.listen(8080, () => {console.log("App rodando!");});
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+})
 
-//teste
+app.post("/salvarpergunta",(req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+});
 
+
+app.listen(8080,()=>{console.log("App rodando!");});
