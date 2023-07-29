f816288a83317fd4b1ea0f4a8dd42019f05c1942
RenanXM <renanxmarques303@gmail.com>
2023-07-29 11:16:35 -0300

    Deleting articles system and articles table  implemented

--- 05blogwithadminpanel_project/articles/ArticlesController.js
+++ 05blogwithadminpanel_project/articles/ArticlesController.js
@@ -6,7 +6,12 @@ const slugify = require("slugify");
 
 
 router.get("/admin/articles", (req, res) => {
-    res.render("admin/articles/index");
+    Article.findAll({
+        // Para poder usar category.title na minha view.
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/index", {articles: articles})
+    });
 });
 
 router.get("/admin/articles/new", (req, res) => {
@@ -28,6 +33,25 @@ router.post("/articles/save", (req, res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     });
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined) {
+        if(!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles");
+            });
+        } else {
+            res.redirect("/admin/articles");
+        }
+    } else {
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router; 
