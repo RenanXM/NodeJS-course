c5548966ecc4b068ad6ed5b931efc376bdd90dc0
RenanXM <renanxmarques303@gmail.com>
2023-07-31 22:23:52 -0300

    Login system basis: Model and Controller start

--- 05blogwithadminpanel_project/index.js
+++ 05blogwithadminpanel_project/index.js
@@ -5,9 +5,11 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./users/UsersController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./users/User");
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -31,6 +33,7 @@ connection
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController);
 
 
 
---
+++ 05blogwithadminpanel_project/users/UsersController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const User = require("./User");
+
+
+router.get("/admin/users", (req, res) => {
+    res.send("Listagem de usuários");
+})
+
+
+router.get("/admin/users/create", (req, res) => {
+    res.render("../views/admin/users/create");
+})
+
+module.exports = router;
