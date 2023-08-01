1c3b1deb4f97b6ec5a9255aac5d207ef967730c2
RenanXM <renanxmarques303@gmail.com>
2023-08-01 11:53:36 -0300

    Middleware, Logout. Finished project

--- 05blogwithadminpanel_project/users/UsersController.js
+++ 05blogwithadminpanel_project/users/UsersController.js
@@ -59,7 +59,7 @@ router.post("/authenticate", (req, res) => {
                     email: user.email
 
                 }
-                res.json(req.session.user);
+                res.redirect("/admin/articles");
 
             }else {
                 res.redirect("/login");
@@ -71,6 +71,9 @@ router.post("/authenticate", (req, res) => {
 });
 
 
-
+router.get("/logout", (req,res) => {
+    req.session.user = undefined;
+    res.redirect("/");
+});
 
 module.exports = router;
